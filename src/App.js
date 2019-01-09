2927d637e006ee6c1165056c1e7881b2fdb53792
Henry Neal <henry.neal0@gmail.com>
2019-01-09 16:54:16 -0500

    mvp almost done, troubleshooting one error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoList from './components/TodoComponents/TodoList.js'
+import TodoForm from './components/TodoComponents/TodoForm.js'
+
+const todoData = [
+  {
+    task: 'Organize Garage',
+    id: Date.now() ,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: Date.now(),
+    completed:false
+  }
+
+]
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      dataList: todoData,
+      task: '',
+      id: Date.now()
+    };
+  }
+  
+ handleChanges = event => {
+   this.setState({ [event.target.task]: event.target.value })
+ }
+
+ addNewTodo = event => {
+   event.preventDefault();
+   this.setState({
+     dataList: [
+       ...this.state.dataList,
+       { task: this.state.task}
+     ],
+     task:''
+   });
+ };
+  
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className='App'>
+        <TodoList todoDataList={this.state.dataList} />
+        <TodoForm 
+          addNewToDo={this.addNewTodo}
+          handleChanges={this.handleChanges}
+          task={this.state.task}
+          id={this.state.id}
+
+        />
       </div>
     );
   }
